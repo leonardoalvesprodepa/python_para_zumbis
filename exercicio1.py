d9e0f2f22eb86afb3318a49ef60e7b6026993011
leonardo.alves <leonardo.alves@prodepa.pa.gov.br>
2016-12-20 11:26:29 -0300

    Exercício 1 e Exercício 2

---
+++ exercicio1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+velocidade = int(input('Digite a velocidade: '))
+limite_velocidade = 110
+valor_multa = 5.00
+multa = 0
+
+if velocidade > limite_velocidade:
+    multa = (velocidadev - limite_velocidadelimite_v) * valor_multa
+    print 'Você foi multado. Sua multa é de R$ %.2f' % multa
